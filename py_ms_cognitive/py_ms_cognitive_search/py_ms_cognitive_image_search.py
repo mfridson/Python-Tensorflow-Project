229a533daec89f304e67bc54e1be389b50697b48
Marc Fridson <marc@ia-bi.com>
2017-03-22 10:08:18 -0400

    Initial commit

---
+++ py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_image_search.py
@@ -0,0 +1,77 @@
+import requests, requests.utils
+from .py_ms_cognitive_search import PyMsCognitiveSearch
+from .py_ms_cognitive_search import QueryChecker
+
+##
+##
+## Image Search
+##
+##
+
+class PyMsCognitiveImageException(Exception):
+    pass
+
+class PyMsCognitiveImageSearch(PyMsCognitiveSearch):
+
+    SEARCH_IMAGE_BASE = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search'
+
+    #def __init__(self, query, api_key='3aae19c115a6466cb0ebbaa7e063e798',silent_fail=False, custom_params=''):
+    def __init__(self, query, api_key='0e171a97111a4e31aa658e32843b90ba',silent_fail=False, custom_params=''):
+        query_url = self.SEARCH_IMAGE_BASE + custom_params
+        PyMsCognitiveSearch.__init__(self, api_key, query, query_url, silent_fail=silent_fail)
+
+    def _search(self, quota, format):
+        '''
+        Returns a list of result objects, with the url for the next page MsCognitive search url.
+        '''
+        quota = min(quota, self.MAX_SEARCH_PER_QUERY)
+        payload = {
+          'q' : self.query,
+          'count' : quota,
+          'offset': self.current_offset,
+          'width': 800,
+          'height': 800,
+          'type':'photograph',
+          'encodingFormat': 'jpeg'
+        }
+        headers = { 'Ocp-Apim-Subscription-Key' : self.api_key }
+        if not self.silent_fail:
+            QueryChecker.check_web_params(payload, headers)
+        response = requests.get(self.QUERY_URL, params=payload, headers=headers)
+
+        json_results = self.get_json_results(response)
+
+        packaged_results = [ImageResult(single_result_json) for single_result_json in json_results["value"]]
+        self.current_offset += min(50, quota, len(packaged_results))
+        return packaged_results
+
+class ImageResult(object):
+    '''
+    The class represents a SINGLE Image result.
+    Each result will come with the following:
+
+    the variable json will contain the full json object of the result.
+
+    conten_url: duration of the Image
+    name: name of the image / page title
+    image_id: image id
+    image_insights_token: image insights token
+    web_search_url: the bing search url for the image
+    host_page_url: the bing url to the host page
+    content_size: size of the image
+    thumbnail_url: url of the thumbnail
+
+    Not included: lots of info, poke in json to see.
+    '''
+
+    def __init__(self, result):
+        self.json = result
+        self.content_url = result.get('contentUrl')
+        self.name = result.get('name')
+        self.image_id = result.get('ImageId')
+        self.image_insights_token = result.get('imageInsightsToken')
+        self.web_search_url = result.get('webSearchUrl')
+        self.host_page_url = result.get('hostPageUrl')
+        self.content_size = result.get('contentSize')
+        self.thumbnail_url= result.get('thumbnailUrl')
+
